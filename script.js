31826ae253bfee53c4b719f16d3e7d1bbc0e4345
unknown <wandilemaluleke@gmail.com>
2021-11-09 10:35:00 +0200

    more names add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let names = ["Wandile", "Khum", "Selina", "Precious", "Andiswe", "Maspeka", "Kagisho", "Vusi", "Mduzi", "Nkosi", "Happy", "Sthembiso", "Ntombea"]
+let names = ["Wandile", "Khum", "Selina", "Precious", "Andiswe", "Maspeka", "Kagisho", "Vusi", "Mduzi", "Nkosi", "Happy", "Sthembiso", "Ntombea", "Sophie", "Lucky"]
 
 let display = document.getElementById("display");
 
